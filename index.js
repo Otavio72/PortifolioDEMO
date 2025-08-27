ba743c2f2c83f35c5f67797a127df5c554c8c21a
Otávio Ribeiro <otavioribeiro1932004@gmail.com>
2025-08-27 10:29:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'static/html')));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, '/static/html/app/index.html'));
